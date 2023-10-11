9783999550eff7ce73b8d93428a4b97b196adf64
Юлия Семенова <julia.sem295@yandex.ru>
2023-10-11 23:23:58 +0300

    kittygram_final

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,9 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = get_random_secret_key()
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
