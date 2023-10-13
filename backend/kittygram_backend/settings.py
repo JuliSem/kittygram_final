f903a841bc0d74d60540de2faa388376bd0cc478
Юлия Семенова <julia.sem295@yandex.ru>
2023-10-13 16:02:22 +0300

    kittygram_final

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,10 +1,7 @@
 # flake8: noqa
 import os
 from django.core.management.utils import get_random_secret_key
-from dotenv import load_dotenv
 from pathlib import Path
-
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
